84be46119ee2e3b6b140f0d2cf338091ff25628f
Jay Shenoy <jayshenoy@hotmail.com>
2017-04-22 15:30:24 -0400

    Update driver and senior registration pages

--- RegAndLog/js/my-app.js
+++ RegAndLog/js/my-app.js
@@ -5,65 +5,20 @@ var myApp = new Framework7();
 var $$ = Dom7;
 
 // Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
+var mainView = myApp.addView('.view-main');
+
+// Senior registration form submission
+$$('#senior-register').on('click', function (e) {
+  var formData = $('#senior-form').serialize();
+  $$.post('http://127.0.0.1:5000/register-senior', formData, function(data) {
+    window.location.href = 'senior-profile.html';
+  });
 });
 
-
-
-$$('#submit').on('click', function(){
-    console.log("Rufus was here");
-    var formData = myApp.formToData('#senior-form');
-    console.log(JSON.stringify(formData));
-    $$.ajax({
-      type: 'POST',
-      url: 'https://flybyapp.herokuapp.com/register-senior',
-      data: formData,
-      contentType: false,
-      cache: false,
-      processData: false,
-      async: false,
-      success: function(data) {
-        console.log("Success");
-        return;
-      }
-    });
-}); 
-
-// Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
-    });
+// Driver registration form submission
+$$('#driver-register').on('click', function (e) {
+  var formData = $('#driver-form').serialize();
+  $$.post('http://127.0.0.1:5000/register-driver', formData, function(data) {
+    window.location.href = 'driver-profile.html';
+  });
 });
-
-// Generate dynamic page
-var dynamicPageIndex = 0;
-function createContentPage() {
-	mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-	return;
-}
