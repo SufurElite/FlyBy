8a2d60c5b40b79a9d53ceaf0518d9246a9e2b3ee
Rufus Behr <rufus.behr@gmail.com>
2017-04-23 00:21:31 -0400

    Added Android capability

---
+++ platforms/android/assets/www/js/my-app.js
@@ -0,0 +1,24 @@
+// Initialize your app
+var myApp = new Framework7();
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main');
+
+// Senior registration form submission
+$$('#senior-register').on('click', function (e) {
+  var formData = $('#senior-form').serialize();
+  $$.post('http://127.0.0.1:5000/register-senior', formData, function(data) {
+    window.location.href = 'senior-profile.html';
+  });
+});
+
+// Driver registration form submission
+$$('#driver-register').on('click', function (e) {
+  var formData = $('#driver-form').serialize();
+  $$.post('http://127.0.0.1:5000/register-driver', formData, function(data) {
+    window.location.href = 'driver-profile.html';
+  });
+});
