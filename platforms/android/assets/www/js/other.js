5c19eb1c1e9d4ce969c1cfe24d833f169b2c7200
Rufus Behr <rufus.behr@gmail.com>
2017-04-28 08:21:10 -0400

    added icons back

--- platforms/android/assets/www/js/other.js
+++ platforms/android/assets/www/js/other.js
@@ -14,6 +14,54 @@ var mainView = myApp.addView('.view-main', {
     domCache: true
 });
 
-var calendarDefault = myApp.calendar({
-    input: '#calendar'
-}); 
+// Variable to store chosen calendar date
+var rideDateTime;
+
+var myCalendar = myApp.calendar({
+    input: '#calendar',
+    onDayClick: function(p, dayContainer, year, month, day) {
+      // Update date when selected from date picker
+      rideDateTime = new Date(year, month, day);
+    }
+});
+
+function destination()
+{
+  // Store ride request timing details
+  var rideRequest;
+
+  // Store current date and time if "Right Now" checked
+  if($('#actualcheckbox').is(':checked')) {
+    rideRequest = {
+      datetime: Date.now()
+    };
+
+    localStorage.setItem('rideRequest', JSON.stringify(rideRequest));
+  } else {
+    // Check if date has been selected
+    if(typeof rideDateTime !== 'undefined') {
+      var hours = parseInt($('#hours').val());
+      // Change hours based on AM/PM
+      var ampm = $('#ampm').val();
+      if(ampm === 'PM')
+        hours += 12;
+
+      var minutes = parseInt($('#minutes').val());
+
+      rideDateTime.setHours(hours);
+      rideDateTime.setMinutes(minutes);
+
+      rideRequest = {
+        datetime: rideDateTime.getTime()
+      };
+
+      localStorage.setItem('rideRequest', JSON.stringify(rideRequest));
+    } else {
+      myApp.alert('Please select a date from the calendar or choose "Right Now"');
+      return;
+    }
+  }
+
+  // Display map
+  window.location.href = "map.html";
+}
